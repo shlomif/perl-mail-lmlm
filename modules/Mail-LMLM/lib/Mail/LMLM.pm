4cc046b47543201693fc4da5c36c8d6705073a93
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-03-03 20:39:27 +0000

    Changed COPYRIGHT to LICENSE to satisfy the kwalitee.

--- modules/Mail-LMLM/lib/Mail/LMLM.pm
+++ modules/Mail-LMLM/lib/Mail/LMLM.pm
@@ -343,7 +343,7 @@ L<Mail::LMLM::Render,1>
 
 Shlomi Fish E<lt>shlomif@vipe.technion.ac.ilE<gt>
 
-=head1 COPYRIGHT AND DISCLAIMER
+=head1 LICENSE
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -363,6 +363,8 @@ IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
+
+(This is the MIT X11 License).
 
 =cut
 
